d261c93120523031b0adbc6799dafabd368a30a3
Utkarsh Choudhary <47511836+utkarshrutgers@users.noreply.github.com>
2019-02-10 22:37:37 -0500

    Update 10 Days of Statistics.py

--- HackerRank/10 Days of Statistics.py
+++ HackerRank/10 Days of Statistics.py
@@ -22,3 +22,18 @@ print(np.median(numbers))
 from scipy.stats import mode
 
 print(int(mode(numbers)[0]))
+
+"""Day0: Find Weighted mean"""
+
+
+n = int(input().strip())
+
+numbers = list(map(int, input().split()))
+freq = list(map(int, input().split()))
+weightedmean = 0.0
+count = 0
+
+weightedmean = sum([a*b for a,b in zip(numbers,freq)])
+weightedmean = round((weightedmean/sum(freq)),1)
+print(weightedmean)
+    
